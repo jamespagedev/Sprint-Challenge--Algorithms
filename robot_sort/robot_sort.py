999e7633a07dd45918e46b73386fc7f5ee4aa86b
jamespagedev <jamespagedev@outlook.com>
2019-03-29 10:47:14 -0700

    test_robot - All Tests including stretch (PASS)

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -97,7 +97,47 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-        pass
+        # rules:
+        #   You may use any pre-defined robot methods.
+        #   You may NOT modify any pre-defined robot methods.
+        #   You may use logical operators. (if, and, or, not, etc.)
+        #   You may use comparison operators. (>, >=, <, <=, ==, is, etc.)
+        #   You may use iterators. (while, for, break, continue)
+        #   You may NOT store any variables. (=) <---------- so how are we supposed to swap elements in an array?
+        #   You may NOT access any instance variables directly. (self._anything)
+        #   You may NOT use any Python libraries or class methods. (sorted(), etc.)
+        #   You may define robot helper methods, as long as they follow all the rules.
+
+        # Edge Case - [] or [#]
+        if not self.can_move_right() and not self.can_move_left():
+            return
+
+        self.set_light_on()
+        # Test Methods:
+            # self.swap_item()
+            # self.move_right()
+            # self.swap_item()
+            # self.move_left()
+            # self.swap_item()
+        if self.compare_item() == None:
+            self.swap_item()
+
+        while self.light_is_on():
+            while self.can_move_right(): # grab the smallest item
+                self.move_right()
+                if self.compare_item() == None and not self.can_move_right():
+                    self.set_light_off()
+                    return
+                elif self.compare_item() == 1 or self.compare_item() == None:
+                    self.swap_item()
+
+            while self.can_move_left(): # replace none with smallest item found
+                self.move_left()
+                if self.compare_item() == None:
+                    self.swap_item()
+                    break
+
+        self.set_light_off()
 
 
 if __name__ == "__main__":
@@ -105,7 +145,6 @@ if __name__ == "__main__":
     # with `python robot_sort.py`
 
     l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
-
     robot = SortingRobot(l)
 
     robot.sort()
